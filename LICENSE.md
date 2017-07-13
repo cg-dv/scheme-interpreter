3708845be616a619027c92d5c6fce40464034c1a
cg-dv <cgdevproject@gmail.com>
2017-07-13 09:59:37 -0500

    added License for repo and Ch 2 code

---
+++ LICENSE.md
@@ -0,0 +1,6 @@
+# License
+
+This repository contains code from the wikibook 
+["Write Yourself A Scheme in 48 Hours"](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours) and code written to answer its exercises. 
+
+"Write Yourself a Scheme in 48 Hours" is a text submitted to [Wikibooks](https://en.wikibooks.org/wiki/Main_Page), and is available under the terms of the [Creative Commons Attribution-ShareAlike License 3.0](https://creativecommons.org/licenses/by-sa/3.0/legalcode). 
---
+++ ch2/ex2_1_1.hs
@@ -0,0 +1,53 @@
+import Control.Monad
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+                first <- letter <|> symbol
+                rest <- many (letter <|> digit <|> symbol) 
+                let atom = first:rest
+                return $ case atom of
+                    "#t" -> Bool True
+                    "#f" -> Bool False 
+                    _    -> Atom atom 
+
+parseNumber :: Parser LispVal
+parseNumber = do
+                  digits <- many1 digit
+                  return $ (Number . read) digits 
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO ()
+main = do
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool 
